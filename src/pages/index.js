7e4db4cb26fd1a92243cfd79a60663da19d2d3b0
olegdeleon <hello@olegdeleon.com>
2018-11-06 00:03:29 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <h2>Design and code React apps</h2>
+    <p>Complete courses on the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
